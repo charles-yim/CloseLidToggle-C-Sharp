72da84fc905f992563b0a2fca58301b1706da9ef
charles-yim <51301223+charles-yim@users.noreply.github.com>
2019-07-29 00:11:01 +0100

    First commit

---
+++ CloseLidToggle/CLT.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CloseLidToggle
+{
+    partial class CLT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CLT));
+            this.btnDoNothing = new System.Windows.Forms.Button();
+            this.btnSleep = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDoNothing
+            // 
+            this.btnDoNothing.Location = new System.Drawing.Point(12, 12);
+            this.btnDoNothing.Name = "btnDoNothing";
+            this.btnDoNothing.Size = new System.Drawing.Size(130, 50);
+            this.btnDoNothing.TabIndex = 0;
+            this.btnDoNothing.Text = "Do Nothing";
+            this.btnDoNothing.UseVisualStyleBackColor = true;
+            this.btnDoNothing.Click += new System.EventHandler(this.btnDoNothing_Click);
+            // 
+            // btnSleep
+            // 
+            this.btnSleep.Location = new System.Drawing.Point(12, 69);
+            this.btnSleep.Name = "btnSleep";
+            this.btnSleep.Size = new System.Drawing.Size(130, 50);
+            this.btnSleep.TabIndex = 1;
+            this.btnSleep.Text = "Sleep";
+            this.btnSleep.UseVisualStyleBackColor = true;
+            this.btnSleep.Click += new System.EventHandler(this.btnSleep_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CloseLidToggle";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // CLT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 131);
+            this.Controls.Add(this.btnSleep);
+            this.Controls.Add(this.btnDoNothing);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CLT";
+            this.Text = "CloseLidToggle";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CLT_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDoNothing;
+        private System.Windows.Forms.Button btnSleep;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ CloseLidToggle/CLT.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CloseLidToggle
+{
+    public partial class CLT : Form
+    {
+        public CLT()
+        {
+            InitializeComponent();
+            notifyIcon1.Icon = Properties.Resources.Icon1;
+        }
+
+        private void runCMD(String cmd)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo processStartInfo = new System.Diagnostics.ProcessStartInfo();
+            processStartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            processStartInfo.FileName = "cmd.exe";
+            processStartInfo.Arguments = "/C " + cmd;
+            process.StartInfo = processStartInfo;
+            process.Start();
+        }
+
+        private void minimiseUI()
+        {
+            this.Hide();
+        }
+
+        //powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+        private void btnDoNothing_Click(object sender, EventArgs e)
+        {
+            runCMD("powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0");
+            runCMD("powercfg -setactive SCHEME_CURRENT");
+        }
+
+        //powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1
+        private void btnSleep_Click(object sender, EventArgs e)
+        {
+            runCMD("powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 1");
+            runCMD("powercfg -setactive SCHEME_CURRENT");
+        }
+
+        private void CLT_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                minimiseUI();
+                e.Cancel = true;
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
